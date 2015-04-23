970e6683e25ae440975e96819c7e20380ab3feeb
alor <alor@hackingteam.it>
2015-04-23 14:57:04 +0200

    whatsapp calls

--- lib/rcs-common/evidence/call.rb
+++ lib/rcs-common/evidence/call.rb
@@ -17,6 +17,7 @@ module CallEvidence
                    0x0148 => :viber,
                    0x0149 => :wechat,
                    0x014a => :line,
+                   0x014b => :whatsapp,
                  }
   
   def decode_additional_header(data)
