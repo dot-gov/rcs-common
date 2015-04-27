c7998b8e47457f054bf980df4c6d5c0956aa1838
alor <alor@hackingteam.it>
2015-04-27 16:05:05 +0200

    typo

--- lib/rcs-common/evidence/proxy.rb
+++ lib/rcs-common/evidence/proxy.rb
@@ -70,7 +70,7 @@ module ProxyEvidence
     info = Hash[common_info]
     info[:data] ||= Hash.new
 
-    # put the encrypted proxed evidence in the content, or parse the other commands
+    # put the encrypted proxied evidence in the content, or parse the other commands
     if info[:data][:type].eql? 'evidence'
       info[:grid_content] = chunks.join
     else
